b7adae941446243d00f57fdb568f7f4ece0975f3
pasindu priyalanka <pasindupriyalanka@gmail.com>
2026-01-19 12:09:10 +0530

    Refactor countdown function

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ function startCountdown(elementId, targetDate) {
         const now = new Date().getTime();
         const difference = target - now;
 
-        // Calculations for days, hours, minutes and seconds
+        
         const d = Math.floor(difference / (1000 * 60 * 60 * 24));
         const h = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
         const s = Math.floor((difference % (1000 * 60)) / 1000);
 
-        // Display results
+        
         document.getElementById(elementId).innerHTML = `${d}d ${h}h ${m}m ${s}s`;
 
-        // If countdown finished
+        
         if (difference < 0) {
             clearInterval(interval);
             document.getElementById(elementId).innerHTML = "EVENT STARTED";
@@ -22,7 +22,7 @@ function startCountdown(elementId, targetDate) {
     }, 1000);
 }
 
-// Set your dates here
+
 startCountdown("exams", "May 20, 2026 09:00:00");
 startCountdown("christmas", "December 25, 2026 00:00:00");
 startCountdown("newyear", "January 1, 2027 00:00:00");
